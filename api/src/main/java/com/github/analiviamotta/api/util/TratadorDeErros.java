127452daad925e6e18118a4c207457fa0cef74d4
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-06-04 18:33:40 -0300

    feat: message error validate in post

--- api/src/main/java/com/github/analiviamotta/api/util/TratadorDeErros.java
+++ api/src/main/java/com/github/analiviamotta/api/util/TratadorDeErros.java
@@ -2,6 +2,8 @@ package com.github.analiviamotta.api.util;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
@@ -9,5 +11,16 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400( MethodArgumentNotValidException ex ) {
+        var erros = ex.getFieldErrors();
+        var lista = erros.stream().map(DadosErroValidacao::new).toList();
+        return ResponseEntity.badRequest().body(lista);
+    }
+    private record DadosErroValidacao(String campo, String msgErro) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
